d2d8ae1c4fef9c1926fd614aa45c7913f4c80926
CelularPrism <46044765+CelularPrism@users.noreply.github.com>
2021-09-11 23:02:43 +0700

    11.09.2021 Localization

---
+++ Anton/Assets/Scripts/Localized/BtnSwitchLang.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class BtnSwitchLang : MonoBehaviour
+{
+    [SerializeField]
+    private LocalizationManager localizationManager;
+
+    void OnButtonClick()
+    {
+        localizationManager.CurrentLanguage = name;
+    }
+}
